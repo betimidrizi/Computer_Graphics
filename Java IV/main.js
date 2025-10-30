f753db855ab8303da22ed3f8694c1a5850087c06
betimidriz <betimidrizi55@gmail.com>
2025-10-30 13:01:26 +0100

    Added light and camera

--- Java IV/main.js
+++ Java IV/main.js
@@ -14,7 +14,8 @@ camera.position.set(0, 0, 5);
 // Shtimi i nje kubi
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMaterial = new THREE.MeshBasicMaterial({
-        color: 0x00ff00
+        color: 0x0000ff
+        
     });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 boxMesh.position.set(-2, 0, -2);
@@ -28,3 +29,13 @@ const box1Material = new THREE.MeshBasicMaterial({
 const box1Mesh = new THREE.Mesh(boxGeometry, box1Material);
 boxMesh.position.set(2, 0, 0);
 scene.add(box1Mesh);
+
+// Shtimi i nje drite
+const light = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light);
+
+const container = document.getElementById('scene1');
+const renderer = new THREE.WebGLRenderer();
+container.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
